287142fa7904ee69dd556ad156b78a3290aa5d42
Tony Young <12010@coderacademy.edu.au>
2022-03-28 21:36:48 +1000

    Removed variables file

--- variables.rb
+++
@@ -1,4 +0,0 @@
-#Variable containing list of main menu items in main menu list
-main_menu_options = CSV.read('main_menu_list.csv') 
-#Instance of Menu class: main menu
-main_menu = Menu.new("main", main_menu_options[0..5])
