c7f56e6122cfdb314ce7121afaaa27c4ebc465c3
Tony Young <12010@coderacademy.edu.au>
2022-03-28 21:35:20 +1000

    Added weather data CSV file

--- main.rb
+++ main.rb
@@ -14,3 +14,20 @@ puts main_menu.display_menu
 puts main_menu.message
 #Saves user selection into variable: user selection
 user_selection = gets.chomp.downcase
+#Case statement to process user input
+case user_selection
+    when menu_options[0][0].downcase
+        weather
+    when menu_options[0][1].downcase
+        puts "tide"
+    when menu_options[0][2].downcase
+        puts "anchorage"
+    when menu_options[0][3].downcase
+        puts "update"
+    when menu_options[0][4].downcase
+        puts "help"
+    when menu_options[0][5].downcase
+        user_wants_to_continue = false
+    else
+        puts "Please enter a valid selection"
+    end
